bcb2823fdd5b5823080865544b32e0288680bd1c
Adeyemi Salau <adeyemisalau@gmail.com>
2019-05-24 17:13:54 +0100

    Third Commit

--- main.go
+++ main.go
@@ -12,13 +12,15 @@ func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
 	rw.Write(markdown)
 }
-func main() {
-	port := os.Getenv("PORT")
 
-	if port == "" {
-		port = "8080"
+func main() {
+
+	portS := os.Getenv("PORT")
+
+	if portS == "" {
+		portS = "8080"
 	}
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(":"+portS, nil)
 }
